e5fb05ab919baa42706fb2f468cb3a1ce412a632
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-04-19 21:04:26 +0100

    first commit

---
+++ Sarah_Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sum_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+                int n = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine("Kindly enter another number");
+                int m = Convert.ToInt32(Console.ReadLine());
+                if (n + m < 100){
+                    Console.WriteLine("true");
+                } else{
+                    Console.WriteLine("false");
+                }
+        }
+    }
+}
---
+++ Sarah_Assignment/Program1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter a number");  
+            int number = Convert.ToInt32(Console.ReadLine());  
+            int result = Check_Prime(number);  
+            if (result == 0)  
+            {  
+                Console.WriteLine("{0} is not a prime number", number);  
+            }  
+            else  
+            {  
+                 Console.WriteLine("{0} is  a prime number", number);  
+            }              
+             
+        }   
+  
+        private static int Check_Prime(int number)  
+       {  
+            int i;  
+            for (i = 2; i <= number - 1; i++)  
+            {  
+                if (number % i == 0)  
+                {  
+                    return 0;  
+                }  
+            }  
+            if (i == number)  
+            {  
+                return 1;  
+            }  
+            return 0;  
+               
+                
+        }  
+    }    
+} 
