a9f533ec60f7d70bbaa1ad126e8fe6e432ac8d4d
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-04-24 22:13:53 +0100

    lab three assignment added

---
+++ LabAssignment3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LabAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Hey there Welcome");
+            Console.WriteLine("This program will help you solve remainder problems");
+		    Console.Write("Enter first number:");
+		    x = int.Parse(Console.ReadLine());
+		    Console.Write("Enter second number:");
+		    y = int.Parse(Console.ReadLine());
+            z = x % y;
+            Console.WriteLine($"The Remainder is: {z}");
+            Console.WriteLine("yeeeepiii!!!!");
+
+
+        }
+    }
+}
